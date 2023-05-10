51021dd04f74adb7c74d3b7fe27438ed54516757
Axel González <axel.gonzalez.dev@gmail.com>
2023-05-09 18:39:46 -0600

    Add default vite js react template
    Includes PWA support

---
+++ client/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg'],
+      manifest: {
+        name: 'My Awesome App',
+        short_name: 'MyApp',
+        description: 'My Awesome App description',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: 'any',
+            type: "image/svg+xml",
+            purpose: "any"
+          },
+        ]
+      }
+    })
+  ],
+})
