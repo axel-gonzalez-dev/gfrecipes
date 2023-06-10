3b93b283923af6e6a0cbe0cd98c4dc703bb5a4b5
Axel González <axel.gonzalez.dev@gmail.com>
2023-06-09 19:42:20 -0600

    Add recipe alpha page
    Also deleting next js default styles

--- client/src/app/recipe/page.tsx
+++ client/src/app/recipe/page.tsx
@@ -1,14 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
 
 const Recipe = () => {
+
+    const [recipes, setRecipes] = useState([]);
+
+    const getRecipes = async () => {
+
+        const response = await fetch("http://localhost:1337/api/recipes")
+        .then(response => response.json())
+        .catch(error => {
+          throw error;
+        });
+    
+        console.log(response);
+
+        setRecipes(response?.data);
+    
+        try{
+    
+        }catch(error){
+          console.error(error);
+        }
+    
+      }
+    
+      useEffect( () => {
+        getRecipes();
+      },[])
 
     return (
         <>
             <main className="p-4">
-                <div className="grid grid-cols-3">
+
+                <div>
+                    {
+                        recipes.map( (recipe,index) => (
+                            <div className="grid grid-cols-3">
                     <div className="max-w-sm rounded overflow-hidden shadow-lg">
                         {/* <img className="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"></img> */}
                         <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                            <div className="font-bold text-xl mb-2">{recipe?.attributes?.title}</div>
                             <p className="text-gray-700 text-base">
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
                             </p>
@@ -21,6 +54,11 @@ const Recipe = () => {
                     </div>
 
                 </div>
+                        ))
+                    }
+                </div>
+                
+                
             </main>
 
         </>
